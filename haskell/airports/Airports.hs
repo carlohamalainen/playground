4d19ef01577d4ff93445c08a17e39fbb192e5635
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-11-26 12:29:31 +1000

    Tidyup.

--- haskell/airports/Airports.hs
+++ haskell/airports/Airports.hs
@@ -21,40 +21,49 @@ instance H.Hashable Grid where
 instance NFData Grid where
     rnf (Grid x y z) = rnf x `seq` rnf y `seq` rnf z
 
+-- Starting grids are based on two airports that Blinco has been to.
 startingGrids :: [Grid]
-startingGrids = concat [ f a b  | a <- airports, b <- airports]
+startingGrids = concat [ f a b  | a <- blinco, b <- blinco]
   where
     f a b = [ Grid (Just a) (Just b) Nothing
             , Grid (Just a) Nothing  (Just b)
             , Grid Nothing  (Just a) (Just b)
             ]
 
+-- Extend any type of 2-row grid in all possible ways.
 extendGrid :: HS.Set String -> Grid -> [Grid]
 extendGrid iata (Grid (Just a) (Just b) Nothing)  = [ Grid (Just a) (Just b) (Just x) | x <- HS.toList iata ]
 extendGrid iata (Grid (Just a) Nothing  (Just b)) = [ Grid (Just a) (Just x) (Just b) | x <- HS.toList iata ]
 extendGrid iata (Grid Nothing  (Just a) (Just b)) = [ Grid (Just x) (Just a) (Just b) | x <- HS.toList iata ]
 extendGrid _    g = error $ "Can't extend this grid: " ++ show g
 
+-- The score of a grid is the number of row and column codes that Blinco has visited.
+-- So the maximum score for a grid is 6.
 score :: Grid -> Integer
 score (Grid (Just a) (Just b) (Just c)) = sum [f x | x <- [a, b, c] ++ verticals [a, b, c]]
   where
-    f x = if x `HS.member` blinco then 1 else 0
+    f x = if x `HS.member` blinco' then 1 else 0
 score g = error $ "Can't compute score of incomplete grid: " ++ show g
 
+-- Given a grid, extend it, check that it is valid, and keep only those
+-- with score 5.
 dostuff :: HS.Set String -> Grid -> [(Integer, Grid)]
 dostuff iataFull x = map (\g -> (score g, g)) goodTriples
   where
     goodTriples = filter isValid $ extendGrid iataFull x
 
+    -- A grid is valid if each row (reading left to right) and each
+    -- column (reading top down) is an IATA code.
     isValid :: Grid -> Bool
     isValid (Grid (Just a) (Just b) (Just c)) = and [v `HS.member` iataFull && length (uniq $ [a, b, c] ++ verticals [a, b, c]) >= 5 | v <- verticals [a, b, c]]
-    isValid _ = False
+    isValid g = error $ "Can't check validity of a partial grid: " ++ show g
 
 uniq :: (Ord t, H.Hashable t) => [t] -> [t]
 uniq = HS.toList . HS.fromList
 
-airports :: [String]
-airports = [ "BNE", "SYD", "ADL", "PER", "CNB", "NTL", "UDG", "CTL"
+-- Airports that Blinco has been to.
+blinco :: [String]
+blinco = [ "BNE", "SYD", "ADL", "PER", "CNB", "NTL", "UDG", "CTL"
            , "ULP", "TWB", "WNR", "BVI", "LRE", "EMD", "BLT", "THG"
            , "ROK", "MKY", "PPP", "TSV", "CNS", "HID", "SIN", "NRT"
            , "PVG", "BKK", "CNX", "KBV", "BOM", "DEL", "UDR", "AMM"
@@ -67,9 +76,11 @@ airports = [ "BNE", "SYD", "ADL", "PER", "CNB", "NTL", "UDG", "CTL"
            , "HBA"
            ]
 
-blinco :: HS.Set String
-blinco = HS.fromList airports
+-- As a hash set for efficient membership testing.
+blinco' :: HS.Set String
+blinco' = HS.fromList blinco
 
+-- Extract the top-down column codes.
 verticals :: [String] -> [String]
 verticals [a, b, c] = [ [a !! 0, b !! 0, c !! 0]
                       , [a !! 1, b !! 1, c !! 1]
@@ -82,6 +93,6 @@ main = do
     -- IATA list taken from http://aircargotracking.skysthelimitsd.com/air-freight-news/international-airport-codes/
     iataFull <- lines <$> readFile "iata.txt"
 
-    let grids = concat $ parMap rdeepseq (dostuff $ HS.fromList iataFull) startingGrids
+    let grids = uniq $ concat $ parMap rdeepseq (dostuff $ HS.fromList iataFull) startingGrids
 
     forM_ (filter (\(s, _) -> s >= 5) grids) $ \(_, Grid (Just x) (Just y) (Just z)) -> putStrLn $ x ++ y ++ z
