0f055f4e870488dda5c15604133af697894fd436
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-11-26 12:20:05 +1000

    Tidyup.

--- haskell/airports/Airports.hs
+++ haskell/airports/Airports.hs
@@ -1,13 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Main where
 
 import Control.DeepSeq
-import Control.Parallel
+import Control.Parallel()
 import Control.Parallel.Strategies
 
 import Control.Applicative ((<$>))
 import Control.Monad (forM_)
-
-import Data.List.Split (chunksOf)
 
 import qualified Data.HashSet as HS
 import qualified Data.Hashable as H
@@ -21,38 +21,34 @@ instance H.Hashable Grid where
 instance NFData Grid where
     rnf (Grid x y z) = rnf x `seq` rnf y `seq` rnf z
 
-fromTwoVisitedAirports :: [Grid]
-fromTwoVisitedAirports = concat [ f a b  | a <- airports, b <- airports]
+startingGrids :: [Grid]
+startingGrids = concat [ f a b  | a <- airports, b <- airports]
   where
     f a b = [ Grid (Just a) (Just b) Nothing
             , Grid (Just a) Nothing  (Just b)
             , Grid Nothing  (Just a) (Just b)
             ]
 
--- extendGrid :: [String] -> Grid -> [Grid]
-extendGrid iata (Grid (Just a) (Just b) Nothing)  = [ (Grid (Just a) (Just b) (Just x)) | x <- HS.toList iata ]
-extendGrid iata (Grid (Just a) Nothing  (Just b)) = [ (Grid (Just a) (Just x) (Just b)) | x <- HS.toList iata ]
-extendGrid iata (Grid Nothing  (Just a) (Just b)) = [ (Grid (Just x) (Just a) (Just b)) | x <- HS.toList iata ]
-extendGrid _ g = error $ "Can't extend this grid: " ++ show g
+extendGrid :: HS.Set String -> Grid -> [Grid]
+extendGrid iata (Grid (Just a) (Just b) Nothing)  = [ Grid (Just a) (Just b) (Just x) | x <- HS.toList iata ]
+extendGrid iata (Grid (Just a) Nothing  (Just b)) = [ Grid (Just a) (Just x) (Just b) | x <- HS.toList iata ]
+extendGrid iata (Grid Nothing  (Just a) (Just b)) = [ Grid (Just x) (Just a) (Just b) | x <- HS.toList iata ]
+extendGrid _    g = error $ "Can't extend this grid: " ++ show g
 
-------------------------
-
-score' :: Grid -> Integer
-score' (Grid (Just a) (Just b) (Just c)) = sum [f x | x <- [a, b, c] ++ verticals [a, b, c]]
+score :: Grid -> Integer
+score (Grid (Just a) (Just b) (Just c)) = sum [f x | x <- [a, b, c] ++ verticals [a, b, c]]
   where
     f x = if x `HS.member` blinco then 1 else 0
-score' g = error $ "Can't compute score of incomplete grid: " ++ show g
+score g = error $ "Can't compute score of incomplete grid: " ++ show g
 
-dostuff' iataFull x = map (\g -> (score' g, g)) goodTriples
+dostuff :: HS.Set String -> Grid -> [(Integer, Grid)]
+dostuff iataFull x = map (\g -> (score g, g)) goodTriples
   where
-    goodTriples = filter isValid $ concat $ map (extendGrid iataFull) fromTwoVisitedAirports
+    goodTriples = filter isValid $ extendGrid iataFull x
 
     isValid :: Grid -> Bool
     isValid (Grid (Just a) (Just b) (Just c)) = and [v `HS.member` iataFull && length (uniq $ [a, b, c] ++ verticals [a, b, c]) >= 5 | v <- verticals [a, b, c]]
     isValid _ = False
-
-------------------------
-
 
 uniq :: (Ord t, H.Hashable t) => [t] -> [t]
 uniq = HS.toList . HS.fromList
@@ -74,13 +70,6 @@ airports = [ "BNE", "SYD", "ADL", "PER", "CNB", "NTL", "UDG", "CTL"
 blinco :: HS.Set String
 blinco = HS.fromList airports
 
-score :: [String] -> Integer
-score [a, b, c] = sum [f x | x <- [a, b, c] ++ verticals [a, b, c]]
-  where
-    f x = if x `HS.member` blinco then 1 else 0
-
-score _ = error "Malformed 3x3 grid?"
-
 verticals :: [String] -> [String]
 verticals [a, b, c] = [ [a !! 0, b !! 0, c !! 0]
                       , [a !! 1, b !! 1, c !! 1]
@@ -88,24 +77,11 @@ verticals [a, b, c] = [ [a !! 0, b !! 0, c !! 0]
                       ]
 verticals _ = error "verticals: malformed 3x3 grid?"
 
-airportPairs = [[a, b] | a <- airports, b <- airports, a < b]
-
-dostuff iataFull x = map (\g -> (score g, g)) goodTriples
-  where
-    goodTriples = filter isValid triples'
-    triples' = concat $ filter (/= []) $ map makeTriple [(ab, x) | ab <- airportPairs]
-
-    makeTriple ([a, b], c) = [[a, b, c]]
-
-    isValid :: [String] -> Bool
-    isValid = \[a, b, c] -> and [v `HS.member` iataFull && length (uniq $ [a, b, c] ++ verticals [a, b, c]) >= 5 | v <- verticals [a, b, c]]
-
+main :: IO ()
 main = do
     -- IATA list taken from http://aircargotracking.skysthelimitsd.com/air-freight-news/international-airport-codes/
     iataFull <- lines <$> readFile "iata.txt"
 
-    -- let x = concat $ parMap rdeepseq (dostuff $ HS.fromList iataFull) iataFull
-    let x = concat $ parMap rdeepseq (dostuff' $ HS.fromList iataFull) iataFull
+    let grids = concat $ parMap rdeepseq (dostuff $ HS.fromList iataFull) startingGrids
 
-    forM_ (filter (\(s, _) -> s >= 5) x) $ \(_, (Grid (Just x) (Just y) (Just z))) -> do
-        putStrLn $ x ++ y ++ z
+    forM_ (filter (\(s, _) -> s >= 5) grids) $ \(_, Grid (Just x) (Just y) (Just z)) -> putStrLn $ x ++ y ++ z
