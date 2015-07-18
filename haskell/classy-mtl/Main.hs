20a57fb06bc35ad92ae6fa9d161a0d693aa9b2c3
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-07-18 20:55:27 +1000

    Tinkering with MTL. Not exactly about the classy thing right now.

---
+++ haskell/classy-mtl/Main.hs
@@ -0,0 +1,51 @@
+module Main where
+
+import Control.Monad.Except
+import Control.Monad.Reader
+
+-- IO then Except then Reader:
+type IoExceptReader = ReaderT Int (ExceptT String IO)
+
+dostuff :: ReaderT Int (ExceptT String IO) Int
+dostuff = do
+    r <- ask
+    if r == 42
+        then throwError "Got 42, oh no"
+        else return $ r + 1000
+
+blah :: IoExceptReader Int
+blah = local (+1) dostuff
+
+-- IO then Reader then Error:
+type IoReaderError = ExceptT String (ReaderT Int IO)
+
+readerThing :: ReaderT Int IO Int
+readerThing = do
+    r <- ask
+    -- maybe get a value from the environment (we can use liftIO here)
+    return $ r + 1
+
+blah' :: IoReaderError Int
+blah' = thisLift $ local (+1) readerThing
+  where
+    -- Explicit type signature so we can see that here, lift is
+    -- taking us from a ReaderT to an ExceptT with a ReaderT inside.
+    thisLift :: ReaderT Int IO Int -> ExceptT String (ReaderT Int IO) Int
+    thisLift = lift
+
+
+main :: IO ()
+main = do
+    e <- runExceptT (runReaderT blah 100)
+
+    case e of
+        Left err    -> putStrLn $ "Error!? " ++ err
+        Right x     -> putStrLn $ "Got value: " ++ show x
+
+    e' <- runReaderT (runExceptT blah') 100
+
+    case e' of
+        Left err'   -> putStrLn $ "Error!? " ++ err'
+        Right x'    -> putStrLn $ "Got value: " ++ show x'
+
+    print ()
---
+++ haskell/classy-mtl/README.md
@@ -0,0 +1,5 @@
+
+
+http://talks.bfpg.org/talks/2015-06-09.next_level_mtl.html
+
+https://github.com/gwils/next-level-mtl-with-classy-optics
