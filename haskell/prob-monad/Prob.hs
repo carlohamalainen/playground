fdab2b8ad6656ba79273b653938ea1202431f35b
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-02-17 22:17:07 +0800

    Probably monad following http://jtobin.ca/encoding-independence-statically

---
+++ haskell/prob-monad/Prob.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Prob where
+
+-- http://jtobin.ca/encoding-independence-statically
+
+import Control.Applicative.Free
+import Control.Monad
+import Control.Monad.Free
+import Control.Monad.Primitive
+import System.Random.MWC.Probability (Prob)
+import qualified System.Random.MWC.Probability as MWC
+
+
+data ProbF r = BetaF      Double Double (Double -> r)
+             | BernoulliF Double        (Bool   -> r)
+             | FlabertF   Double Double (Double -> r)
+             deriving Functor
+
+type Model = Free ProbF
+
+beta :: Double -> Double -> Model Double
+beta a b = liftF $ BetaF a b id
+
+bernoulli :: Double -> Model Bool
+bernoulli p = liftF $ BernoulliF p id
+
+flabert :: Double -> Double -> Model Double
+flabert a b = liftF $ FlabertF a b id
+
+coin :: Double -> Double -> Model Bool
+coin a b = do
+    x <- beta a b
+    bernoulli x
+
+eval = iterM $ \case
+    BetaF a b k     -> MWC.beta a b    >>= k
+    BernoulliF p k  -> MWC.bernoulli p >>= k
+    FlabertF a b k  -> do z <- MWC.beta a b
+                          let z' = if z < 0 then 2*z else 3*z
+                          k z'
+coin' :: Double -> Double -> Model Bool
+coin' a b = do
+    x <- flabert a b
+    bernoulli x
+
+go1 :: IO ()
+go1 = do
+    gen <- MWC.createSystemRandom
+    replicateM 10 (MWC.sample (eval $ coin 1 1) gen) >>= print
+
+go2 :: IO ()
+go2 = do
+    gen <- MWC.createSystemRandom
+    replicateM 10 (MWC.sample (eval $ coin' 1 1) gen) >>= print
+
+
+
+
